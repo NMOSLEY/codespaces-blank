933976c8d923362599fbdc01db581b4f8b487372
NMOSLEY <kiranredhood45@icloud.com>
2025-01-11 19:40:03 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def user_input():
+	name = input("Enter your name: ")
+	print('Hello', name)
+favorite = input("user_input")
+print('What is your favorite franchise?', favorite)
+for i in range(1):
+        print('I love', favorite)
+        
